2d34a68ed831b31042800c72cccc0567751df984
Darrkoh <157045831+Darrkoh@users.noreply.github.com>
2024-04-16 01:03:37 +0100

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,15 @@
+function Youtube() {
+    window.location.href = "https://www.youtube.com/channel/UC3Ez5knk3Op7ddzahFEt7ng"; //Used to redirect to Youtube
+}
+
+function Twitch() {
+    window.location.href = "https://www.twitch.tv/darrkkoh"; //Used to redirect to Twitch
+}
+
+function GitHub() {
+    window.location.href = "https://github.com/Darrkoh"; //Used to redirect to GitHub
+}
+
+function MAL() {
+    window.location.href = "https://myanimelist.net/profile/Darrkoh"; //Used to redirect to MAL
+}
